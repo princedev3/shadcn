0a37d609fa7f7354f938aa2f8f8775d2e8f64ba2
princedev3 <marvinprince232@gmail.com>
2025-03-21 10:02:56 +0200

    console

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,9 +6,9 @@ import React from "react";
 const page = async () => {
   const session = await auth();
   console.log(session);
-  // if (session) {
-  //   redirect("/");
-  // }
+  if (session) {
+    redirect("/");
+  }
   return (
     <div className="">
       <form
