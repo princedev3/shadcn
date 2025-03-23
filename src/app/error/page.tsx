058c9abdbf45047c052b2fcdea9e6c10e7d4e8ac
princedev3 <marvinprince232@gmail.com>
2025-03-23 16:09:10 +0200

    error page

---
+++ src/app/error/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { useRouter } from "next/router";
+
+const ErrorPage = () => {
+  const router = useRouter();
+  const { error } = router.query;
+
+  return (
+    <div>
+      <h1>Error during Sign-In</h1>
+      <p>{error ? error : "An unexpected error occurred. Please try again."}</p>
+      <a href="/login">Go back to login</a>
+    </div>
+  );
+};
+
+export default ErrorPage;
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -47,6 +47,9 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
       },
     }),
   ],
+  pages: {
+    error: "/error",
+  },
   callbacks: {
     async signIn({ user, account }) {
       if (account?.provider === "google") {
