d4f337bc278bbc29c3532e8d1771926dc2d28514
princedev3 <marvinprince232@gmail.com>
2025-03-23 12:42:55 +0200

    checks

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -1,16 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export const POST = async (req: NextRequest) => {
-  try {
-    const baseUrl = new URL(req.url).origin; // Get the base URL dynamically
-    const redirectUrl = `${baseUrl}/login`;
-    return NextResponse.redirect(redirectUrl, {
-      headers: {
-        "Set-Cookie": `authjs.session-token=; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=0`,
-      },
-    });
-  } catch (error) {
-    console.error("Logout error:", error);
-    return NextResponse.json({ message: "Logout failed" }, { status: 500 });
-  }
-};
+export async function POST() {
+  const response = new NextResponse("Logged out", { status: 200 });
+
+  response.headers.append(
+    "Set-Cookie",
+    `authjs.session-token=; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=0`
+  );
+
+  return response;
+}
