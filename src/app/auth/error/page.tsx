6ea7862fcef2fc808a556a2c16758405ca233f97
princedev3 <marvinprince232@gmail.com>
2025-03-23 14:47:10 +0200

    error page

--- src/app/auth/error/page.tsx
+++ src/app/auth/error/page.tsx
@@ -1,7 +1,18 @@
+"use client";
 import React from "react";
+import { useRouter } from "next/router";
 
 const page = () => {
-  return <div>page</div>;
+  const router = useRouter();
+  const { error } = router.query;
+
+  return (
+    <div>
+      <h1>Error during Sign-In</h1>
+      <p>{error ? error : "An unexpected error occurred. Please try again."}</p>
+      <a href="/login">Go back to login</a>
+    </div>
+  );
 };
 
 export default page;
